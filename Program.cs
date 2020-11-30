241328cf651073090ebd5238e259c42b06a9aa94
InfiniAccess <sgschreuder@gmail.com>
2020-11-30 17:38:27 -0600

    First Commit

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BoxingUnboxing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<object> Boxes = new List<object>();
+
+            Boxes.Add(7);
+            Boxes.Add(28);
+            Boxes.Add(-1);
+            Boxes.Add(true);
+            Boxes.Add("chair");
+
+            int sum = 0;
+            foreach (object item in Boxes)
+            {
+                if (item is int)
+                {
+                    sum += (int)item;
+                }
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
